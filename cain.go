da626454d2ff5fbafad8b17df417ca52680130a5
Aaron Bager <aaronbager@protonmail.com>
2025-12-09 01:14:42 -0500

    Adding .gitignore, adding loop on retrieving character data

--- cain.go
+++ cain.go
@@ -8,34 +8,66 @@ import (
 	yaml "go.yaml.in/yaml/v4"
 )
 
-func getCharacterData(filename string) []byte {
+func getCharacterData(filename string) ([]byte, error) {
 	characterData, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		return nil, err
 	}
-	return characterData
+	return characterData, nil
 }
 
 func main() {
-	var character Character
-	if len(os.Args) > 1 {
+	var characterData []byte // Holds raw character data before unmarshalling into struct
+	var err error            // holds error
+	var character Character  // struct to hold character data
+	var userInput tea.Model
+
+	if len(os.Args) > 1 { // If filename is provided at launch
 		filename := os.Args[1]
-		characterData := getCharacterData(filename)
-		err := yaml.Unmarshal(characterData, &character)
+		characterData, err = getCharacterData(filename)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		err = yaml.Unmarshal(characterData, &character)
 		if err != nil {
 			log.Fatalf("cannot unmarshal data: %v", err)
+			os.Exit(2)
 		}
-	} else {
+	} else { // if filename is not provided at launch
 		p := tea.NewProgram(initialModel())
-		userInput, err := p.Run()
-		if err != nil {
+		userInput, err = p.Run()
+		if err != nil { // if bubbletea program fails
 			log.Fatal(err)
 			os.Exit(2)
 		} else {
 			userInputData := userInput.(model)
-			characterData := getCharacterData(userInputData.textInput.Value())
-			err := yaml.Unmarshal(characterData, &character)
+			characterData, err = getCharacterData(userInputData.textInput.Value())
+			if err != nil { // if file is not opened
+				if err == os.ErrNotExist { // if file is not opened because it is not found
+					for { // loop until user provides a valid filename
+						p = tea.NewProgram(initialModel())
+						userInput, err = p.Run()
+						if err != nil { // if bubbletea program fails
+							log.Fatal(err)
+							os.Exit(2)
+						} else {
+							userInputData = userInput.(model)
+							characterData, err = getCharacterData(userInputData.textInput.Value())
+							if err != nil { // if file is not opened
+								if err == os.ErrNotExist { // if file is not opened because it is not found
+									continue
+								}
+								log.Fatal(err)
+								os.Exit(2)
+							}
+							break
+						}
+
+					}
+				}
+			}
+			err = yaml.Unmarshal(characterData, &character)
 			if err != nil {
 				log.Fatalf("cannot unmarshal data: %v", err)
 			}
